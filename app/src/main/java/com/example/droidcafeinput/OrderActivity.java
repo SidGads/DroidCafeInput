fecfdcc6bc699bebe831b6e55bdf7a3932f54fd1
SidGads <sidneygad@hotmail.com>
2019-09-23 14:45:30 -0400

    I dont even know

--- app/src/main/java/com/example/droidcafeinput/OrderActivity.java
+++ app/src/main/java/com/example/droidcafeinput/OrderActivity.java
@@ -18,16 +18,56 @@ package com.example.droidcafeinput;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.RadioButton;
+import android.widget.Toast;
+import android.widget.AdapterView;
 
 /**
  * This activity is blank for now. Subsequent versions of the app
  * provide input controls and a delivery method for an order.
  */
-public class OrderActivity extends AppCompatActivity {
+public class OrderActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+        switch (view.getId()) {
+            case R.id.sameday:
+                if (checked)
+                    displayToast(getString(R.string.same_day));
+                break;
+            case R.id.nextday:
+                if (checked)
+                    displayToast(getString(R.string.next_day));
+                break;
+            case R.id.pickup:
+                if (checked)
+                    displayToast(getString(R.string.pick_up));
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
